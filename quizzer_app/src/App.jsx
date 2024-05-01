b4bc160ede0b153dddd5d7f745e2bba4a3a35f1c
Janphan <janphan061@gmail.com>
2024-05-01 18:52:58 +0300

    list of published quizzes - front end

--- quizzer_app/src/App.jsx
+++ quizzer_app/src/App.jsx
@@ -1,14 +1,25 @@
-import { useState } from 'react'
-import './App.css'
-import QuizzList from './components/student/GetPublishedQuizz'
+import Container from '@mui/material/Container';
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+
+import PublishedQuizz from './components/student/PublishedQuizz.jsx'
 
 function App() {
 
-  return (
-    <>
-     <QuizzList/>
-    </>
-  )
+    return (
+        <>
+            <Container maxWidth="xl">
+                <AppBar position="static">
+                    <Toolbar>
+                        <Typography variant="h6">Quizzer</Typography>
+                    </Toolbar>
+                </AppBar>
+                <PublishedQuizz/>
+            </Container>
+
+        </>
+    )
 }
 
 export default App
