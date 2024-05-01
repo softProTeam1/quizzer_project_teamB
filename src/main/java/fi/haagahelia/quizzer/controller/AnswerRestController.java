a8b79fed18fa32bab824cafafd6f61081f00a0b5
Makotosoul <minhthien.ng4612@gmail.com>
2024-05-01 23:23:45 +0300

    delete minior unused import signed by: Thien Nguyen

--- src/main/java/fi/haagahelia/quizzer/controller/AnswerRestController.java
+++ src/main/java/fi/haagahelia/quizzer/controller/AnswerRestController.java
@@ -1,13 +1,9 @@
 package fi.haagahelia.quizzer.controller;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
-import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
