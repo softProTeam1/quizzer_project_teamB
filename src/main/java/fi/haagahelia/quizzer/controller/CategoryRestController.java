465d39b3681e8950b80cf1a9b9d747b8e0967329
anhng1106 <ntvanh1106@gmail.com>
2024-05-02 12:26:30 +0300

    Added annotation description to CategoryRestController

--- src/main/java/fi/haagahelia/quizzer/controller/CategoryRestController.java
+++ src/main/java/fi/haagahelia/quizzer/controller/CategoryRestController.java
@@ -6,6 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 
 import fi.haagahelia.quizzer.model.Category;
 import fi.haagahelia.quizzer.repository.CategoryRepository;
@@ -16,7 +19,11 @@ public class CategoryRestController {
     @Autowired
     private CategoryRepository categoryRepository;
 
-
+    @Operation(summary = "List all categories",
+            description = "Retrieves a list of all categories sorted in ascending order by their names.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully retrieved the list of categories")
+    })
     @GetMapping("")
     public List<Category> CategoriesList() {
         List<Category> categories = categoryRepository.findAllByOrderByNameAsc();
