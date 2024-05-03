c681f5fd8e89df6b5bfcbf02f29283c31b52cedb
anhng1106 <ntvanh1106@gmail.com>
2024-05-03 11:25:41 +0300

    Minimizing task display published quizz list to student

---
+++ quizzer_app/src/components/fetchapi.jsx
@@ -0,0 +1,25 @@
+import {useState} from "react";
+
+// Define BACKEND_URL
+const BACKEND_URL = "http://localhost:8080";
+
+// Define a custom React hook that uses useState and fetches data
+export function useGetPublishedQuizzes() {
+    const [quizz, setQuizz] = useState([]);
+
+    // Define an async function to fetch quizzes
+    const fetchQuizzes = async () => {
+        try {
+            const response = await fetch(`${BACKEND_URL}/api/quizzer/publishedquizz`);
+            if (!response.ok) {
+                throw new Error('Network response was not ok: ' + response.statusText);
+            }
+            const data = await response.json();
+            setQuizz(data);
+        } catch (err) {
+            console.error('Fetch error:', err);
+        }
+    };
+
+    return {quizz, fetchQuizzes};
+}
