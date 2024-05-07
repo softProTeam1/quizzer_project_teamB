b8b3e10a4c337c79d2f389007f97524bb0a22abe
Janphan <janphan061@gmail.com>
2024-05-07 20:59:46 +0300

    add UserController + UserDetailServiceImpl + SignupForm

--- src/main/java/fi/haagahelia/quizzer/config/WebSecurityConfig.java
+++ src/main/java/fi/haagahelia/quizzer/config/WebSecurityConfig.java
@@ -1,4 +1,4 @@
-package fi.haagahelia.quizzer;
+package fi.haagahelia.quizzer.config;
 
 
 import org.springframework.context.annotation.Bean;
@@ -8,6 +8,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -27,7 +28,7 @@ public class WebSecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf(csrf -> csrf.disable()).cors(withDefaults())
+        http.csrf(AbstractHttpConfigurer::disable).cors(withDefaults())
                 .sessionManagement(
                         sessionManagement -> sessionManagement
                                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
---
+++ src/main/java/fi/haagahelia/quizzer/controller/UserDetailServiceImpl.java
@@ -0,0 +1,26 @@
+package fi.haagahelia.quizzer.controller;
+
+import fi.haagahelia.quizzer.model.AppUser;
+import fi.haagahelia.quizzer.repository.AppUserRepository;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailServiceImpl implements UserDetailsService {
+    private final AppUserRepository repository;
+
+    public UserDetailServiceImpl(AppUserRepository appUserRepository) {
+        this.repository = appUserRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        AppUser curruser = repository.findByUsername(username);
+        UserDetails user = new org.springframework.security.core.userdetails.User(username, curruser.getPasswordHash(),
+                AuthorityUtils.createAuthorityList(curruser.getRole()));
+        return user;
+    }
+}
