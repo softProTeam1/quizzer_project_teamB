0ccb18826efa066957741c18ead294e0dcf64670
Janphan <janphan061@gmail.com>
2024-05-07 20:37:15 +0300

    add webSecurityConfig

---
+++ src/main/java/fi/haagahelia/quizzer/WebSecurityConfig.java
@@ -0,0 +1,59 @@
+package fi.haagahelia.quizzer;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf -> csrf.disable()).cors(withDefaults())
+                .sessionManagement(
+                        sessionManagement -> sessionManagement
+                                .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authorizeHttpRequests -> authorizeHttpRequests
+                        .requestMatchers(
+                                antMatcher(HttpMethod.POST, "/api/auth/login"),
+                                antMatcher(HttpMethod.POST, "/api/users"),
+                                antMatcher(HttpMethod.GET, "/api/messages"),
+                                antMatcher(HttpMethod.GET, "/api/messages/*"),
+                                antMatcher("/error"),
+                                // Swagger paths
+                                antMatcher(HttpMethod.GET, "/v3/api-docs/**"),
+                                antMatcher(HttpMethod.GET, "/configuration/ui"),
+                                antMatcher(HttpMethod.GET, "/swagger-resources/**"),
+                                antMatcher(HttpMethod.GET, "/configuration/security"),
+                                antMatcher(HttpMethod.GET, "/swagger-ui/**"))
+                        .permitAll()
+                        .anyRequest()
+                        .authenticated());
+
+
+        return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
+        return authConfig.getAuthenticationManager();
+    }
+}
