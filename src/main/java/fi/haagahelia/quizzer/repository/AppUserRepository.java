3b5fbd49582820991f76404fb7b5c670ea8917ef
Janphan <janphan061@gmail.com>
2024-05-07 20:20:57 +0300

    add AppUser ad AppUserRepository

---
+++ src/main/java/fi/haagahelia/quizzer/repository/AppUserRepository.java
@@ -0,0 +1,9 @@
+package fi.haagahelia.quizzer.repository;
+
+import fi.haagahelia.quizzer.model.AppUser;
+import org.springframework.data.repository.CrudRepository;
+
+public interface AppUserRepository extends CrudRepository<AppUser, Long> {
+    AppUser findByUsername(String username);
+}
+
