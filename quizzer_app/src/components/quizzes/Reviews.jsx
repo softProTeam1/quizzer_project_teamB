9d367e8cb4ed4ee26d39dd4d87893e7b483c00ad
anhng1106 <ntvanh1106@gmail.com>
2024-05-10 16:40:22 +0300

    Changed quizzId into quizz.name in Reviews.jsx file

--- quizzer_app/src/components/quizzes/Reviews.jsx
+++ quizzer_app/src/components/quizzes/Reviews.jsx
@@ -1,24 +1,23 @@
-import React, { useState } from "react";
+import React, {useEffect, useState} from "react";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 
 import {Link, useParams} from "react-router-dom";
+import {getQuizById} from "../fetchapi.jsx";
 
 function Reviews() {
     const { quizzId } = useParams(); // Get the quizzId from the URL params
+	const { quiz, fetchQuiz} = getQuizById(quizzId);
 
+	//fetch the data on load
+	useEffect(() => {
+		fetchQuiz();
+	});
 
-
-
-   
-    
     return (
         <div>
-            <Typography variant="h4">Reviews for Quizz {quizzId}</Typography>
-
-         
-				
-					<div>
+            <Typography variant="h4">Reviews for Quizz "{quiz.name}"</Typography>
+		<div>
                         
 						<Link to={`/quizzer/addreview/quizz/${quizzId}`}>
 							<Button >
