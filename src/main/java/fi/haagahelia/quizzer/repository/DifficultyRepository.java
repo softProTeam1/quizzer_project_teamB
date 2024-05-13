e0524848eb739ca931609d6e0e006300874f5b85
Makotosoul <minhthien.ng4612@gmail.com>
2024-05-13 10:00:44 +0300

    deleting the rest of the unused import

--- src/main/java/fi/haagahelia/quizzer/repository/DifficultyRepository.java
+++ src/main/java/fi/haagahelia/quizzer/repository/DifficultyRepository.java
@@ -3,7 +3,6 @@ package fi.haagahelia.quizzer.repository;
 import fi.haagahelia.quizzer.model.Difficulty;
 import org.springframework.data.repository.CrudRepository;
 
-import java.util.List;
 
 public interface DifficultyRepository extends CrudRepository<Difficulty, Long> {
 
--- src/test/java/fi/haagahelia/quizzer/controller/CategoryRestControllerTest.java
+++ src/test/java/fi/haagahelia/quizzer/controller/CategoryRestControllerTest.java
@@ -1,16 +1,7 @@
 package fi.haagahelia.quizzer.controller;
 
-import static org.hamcrest.Matchers.hasItem;
-import static org.hamcrest.Matchers.hasSize;
-import static org.hamcrest.Matchers.not;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -18,7 +9,6 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import fi.haagahelia.quizzer.model.Category;
 import fi.haagahelia.quizzer.repository.CategoryRepository;
 
 @SpringBootTest
