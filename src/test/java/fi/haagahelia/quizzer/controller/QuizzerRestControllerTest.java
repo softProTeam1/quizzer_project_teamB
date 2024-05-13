d64b842c5fb92789547b60707135394beb8fea24
Janphan <janphan061@gmail.com>
2024-05-13 09:37:32 +0300

    delete controler-typo

--- src/test/java/fi/haagahelia/quizzer/controller/QuizzerRestControllerTest.java
+++ src/test/java/fi/haagahelia/quizzer/controller/QuizzerRestControllerTest.java
@@ -155,4 +155,41 @@ public class QuizzerRestControllerTest {
                 .andExpect(status().isNotFound());
     }
 
+    @Test
+    public void getAllCategoriesReturnsEmptyListWhenNoCategoriesExist() throws Exception {
+
+        // Act
+        this.mockMvc.perform(get("/api/category"))
+
+                // Assert
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(0)));
+    }
+
+    @Test
+    public void getAllCategoriesReturnsListOfCategoriesWhenCategoryExist() throws Exception {
+
+        // Arrange
+        Category category1 = new Category("Test Category Name 1", "Test Category Description");
+        Category category2 = new Category("Test Category Name 2", "Test Category Description");
+        Category category3 = new Category("Test Category Name 3", "Test Category Description");
+        categoryRepository.saveAll(List.of(category1, category2, category3));
+
+        // Act
+        this.mockMvc.perform(get("/api/category"))
+
+                // Assert
+                .andExpect(status().isOk())
+
+                .andExpect(jsonPath("$", hasSize(3)))
+                .andExpect(jsonPath("$[0].name").value("Test Category Name 1"))
+                .andExpect(jsonPath("$[0].description").value("Test Category Description"))
+
+                .andExpect(jsonPath("$[1].name").value("Test Category Name 2"))
+                .andExpect(jsonPath("$[1].description").value("Test Category Description"))
+
+                .andExpect(jsonPath("$[2].name", not(hasItem("Test Category Name 3"))))
+                .andExpect(jsonPath("$[2].description").value("Test Category Description"));
+    }
+
 }
